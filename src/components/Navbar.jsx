050f973bc25ed7dbeefd1723d8c48c3b7819d386
cristianbyte <charlsmaritz@gmail.com>
2022-11-11 12:51:06 -0500

    update readme.md and navegation with css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,23 @@ import React,{useState} from 'react';
 
 export default function Navbar() {
     const [isOn, setIsOn] = useState(false)
+    const [nav, setNav] = useState(false)
+
+    const changeBackground = () =>{
+        if(window.scrollY >= 50 ){
+            setNav(true);
+        }else{
+            setNav(false)
+        }
+    }
+
     function buttonClick(){
         setIsOn(!isOn)
     }
+    window.addEventListener('scroll',changeBackground)
 
     return (
-        <nav className='m__nav'>
+        <nav className={nav ? 'm__nav navActive' : 'm__nav'}>
             <div className='m__logo'>mentor</div>
             <ul className={`${isOn ? 'm__nav-listActive' : ''} m__nav-list `} >
                 <a href='#' className='m__nav-list--i'>disciplinas</a>
